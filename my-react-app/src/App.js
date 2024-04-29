1532f014cfcc51447cde2c5ada8010a20a3b166e
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-04-29 10:48:16 +0200

    Job 03 Finish

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Button from './components/Button/Button.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Button text="Je suis un bouton" onClick={() => console.log("Bouton cliqué !")} />
+        </div>
+    );
 }
 
 export default App;
