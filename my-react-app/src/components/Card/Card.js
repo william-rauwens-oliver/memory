8074ee5cc1976d1248f6542486e2db33a5498a9a
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-04-29 16:36:59 +0200

    add App.js and fonction to return card

--- my-react-app/src/components/Card/Card.js
+++ my-react-app/src/components/Card/Card.js
@@ -1,17 +1,15 @@
 // Card.js
+
 import React from 'react';
-import './Card.css'; // CSS pour les styles des cartes
+import './Card.css'; // Assurez-vous d'avoir une feuille de style Card.css pour la classe 'hidden'
 
 const Card = ({ id, symbol, flipped, onClick, disabled }) => {
   return (
     <div
-      className={`card ${flipped ? 'flipped' : ''}`}
+      className={`card ${flipped ? '' : 'hidden'}`} // Ajoutez la classe 'hidden' si la carte n'est pas retournée
       onClick={() => (disabled ? null : onClick(id))}
     >
-      <div className="inner">
-        <div className="front"></div>
-        <div className="back">{symbol}</div>
-      </div>
+      {flipped ? <span className="symbol">{symbol}</span> : null} {/* Afficher la lettre uniquement si la carte est retournée */}
     </div>
   );
 };
